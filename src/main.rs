1bb1d92756a80d3ac29c77718a9bc98e3a07a25d
Vitor Capretz <capretzvitor@gmail.com>
2023-06-30 15:03:09 -0300

    health check and basic test setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!!");
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
