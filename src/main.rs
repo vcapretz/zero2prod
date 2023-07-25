faef6d3fec151450f125fc8266719b25a3314539
Vitor Capretz <capretzvitor@gmail.com>
2023-07-25 15:13:36 -0300

    connect to db

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
